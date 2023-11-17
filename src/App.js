212729712c385f04d84b2de5b56952b744efcb44
Mukku <mukkuraj0499@gmail.com>
2023-11-17 23:03:59 +0530

    update code

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./App.css";
 
 
-// **********30:00
+
 function App() {
 
   const[count,setCount] =useState(0);
 
   function decreaseHandler(){
   setCount(count-1);
-  }
+  } 
 
   function increaseHandler(){
   setCount(count+1);
@@ -19,22 +19,24 @@ function App() {
     setCount(0);
   }
 
+
+
   return (
    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#0f1d2d] flex-col gap-10">
 
     <div className="text-[#0398d4] font-medium text-2xl">Increment & Decrement </div>
     
-    <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] ">
+    <div className="bg-white flex justify-center gap-12 py-3 px-2 rounded-sm text-[25px] text-[#344151] ">
 
-      <button onClick={decreaseHandler} className="border-r-2 text-center  w-20 border-[#bfbfbf]  text-5xl">
+      <button onClick={decreaseHandler} className="border-r-2 border-l-4 text-center  w-20 border-[#bfbfbf]  text-5xl">
         -
       </button>
 
-      <div className="font-bold gap-10  text-5xl"> 
+      <div className="font-bold gap-10  text-5xl">
      {count}
       </div>
 
-      <button onClick={increaseHandler} className="border-r-2 text-center  w-20 border-[#bfbfbf]  text-5xl">
+      <button onClick={increaseHandler} className="border-l-2 border-r-4 text-center  w-20 border-[#bfbfbf]  text-5xl">
         + 
       </button> 
 
